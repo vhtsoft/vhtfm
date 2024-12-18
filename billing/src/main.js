fb1d2e2ea2b3fd6e88cd62ed47eedc6ffa68f6bf
Do Ngoc Tu <tdngoctu@gmail.com>
2024-12-19 00:50:21 +0700

    fix js

--- billing/src/main.js
+++ billing/src/main.js
@@ -1,14 +1,14 @@
 import './index.css'
 
 import { createApp } from 'vue'
-import { VhtfmUI, setConfig, frappeRequest } from 'frappe-ui'
+import { FrappeUI, setConfig, frappeRequest } from 'frappe-ui'
 import router from './router'
 import App from './App.vue'
 
 let app = createApp(App)
 
 setConfig('resourceFetcher', frappeRequest)
-app.use(VhtfmUI)
+app.use(FrappeUI)
 app.use(router)
 
 app.mount('#app')
